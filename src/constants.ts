5c77ad75f7bed4b9a7666ec614b6a8678cbe69c0
cpesar <cpesar1@gmail.com>
2025-01-26 17:40:25 -0700

    added blog posts and main layout to pages

---
+++ src/constants.ts
@@ -0,0 +1,4 @@
+export const SITE_TITLE = 'TechPeople Blog';
+export const SITE_DESCRIPTION = 'Articles stores and tutorials for developers';
+export const HOMEPAGE_ARTICLES_LIMIT = 7;
+export const ARTICLES_PER_PAGE = 6;
